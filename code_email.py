ee4e9bdf973382173d007b360786e99932f18a8d
Aman Singh - Xcriminal <aaryanamanaa19@gmail.com>
2024-10-27 23:34:06 +0530

    Refactor email sending function to include SMTP authentication

--- code_email.py
+++ code_email.py
@@ -9,4 +9,8 @@ def Email_send_function(to, subject, message, uname, pasw):
     msg = "Subject: {}\n\n{}".format(subject, message)
     s.sendmail(uname, to, msg)
     x = s.ehlo()
-
+    if x[0] == 250:
+        return "s"
+    else:
+        return "f"
+    s.close()
